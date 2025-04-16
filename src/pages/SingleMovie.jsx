b571a8c15fd970d43f09f852fb99c8cb137e38a5
mattiacoli <mattia.coli@aol.com>
2025-04-16 16:07:41 +0200

    Enhance styling of Jumbotron and movie image in SingleMovie component

--- src/pages/SingleMovie.jsx
+++ src/pages/SingleMovie.jsx
@@ -24,10 +24,10 @@ export default function SingleMovie() {
   return (
     <>
       {/* Jumbotron */}
-      <div className="jumbotron p-5 mb-4 bg-black text-white border-bottom rounded-bottom" >
-        <div className="container-fluid py-5">
+      <div className="jumbotron p-5 mb-4 bg-black text-white border-bottom rounded-bottom" style={{ backgroundImage: `url(http://localhost:3000/images/${movie.image})`, backgroundSize: 'cover', opacity: 1 }} >
+        <div className="container-fluid p-5 rounded-3" style={{ backgroundColor: ' rgba(0, 0, 0, 0.85) ' }}>
 
-          <div className="row">
+          <div className="row" >
             <div className="col-8 fs-3">
               <h1 className="display-5 fw-bolder mb-3">{movie.title}</h1>
               <p>Director: {movie.director}</p>
@@ -41,7 +41,7 @@ export default function SingleMovie() {
             </div>
 
             <div className="col-4">
-              <img src={`http://localhost:3000/images/${movie.image}`} alt="" className='img-fluid' />
+              <img src={`http://localhost:3000/images/${movie.image}`} alt="" className='img-fluid border rounded-3' />
             </div>
           </div>
 
