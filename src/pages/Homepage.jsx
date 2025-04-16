ce9cce81b241e3fc6f8e09b54f6af67a97274181
mattiacoli <mattia.coli@aol.com>
2025-04-16 12:48:45 +0200

    Add Barlow Condensed font and enhance movie display in Homepage and SingleMovie components

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
 export default function Homepage() {
 
@@ -18,33 +19,32 @@ export default function Homepage() {
   return (
     <>
 
-      <div className="container mt-3">
-        <div className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4">
-          {movies.length > 0 && (
-            movies.map(movie => (
 
-              <div className="col" key={movie.id}>
-                <div className="card h-100">
-                  <img src={`http://localhost:3000/images/${movie.image}`} alt="" style={{ aspectRatio: 1, objectFit: 'fill' }} />
-                  <div className="card-body">
-                    <h3>{movie.title}</h3>
-                    <p>{movie.genre}</p>
+
+      {/* Movies Card Section */}
+      <section>
+
+        <div className="container mt-3">
+          <div className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4">
+            {movies.length > 0 && (
+              movies.map(movie => (
+
+                <div className="col" key={movie.id}>
+                  <div className="card h-100">
+                    <Link to={`/movie/${movie.id}`}>
+                      <img src={`http://localhost:3000/images/${movie.image}`} alt="" style={{ aspectRatio: 1, objectFit: 'fill' }} className="card-img-top" />
+                    </Link>
+                    <div className="card-body">
+                      <h3>{movie.title}</h3>
+                      <p>{movie.genre}</p>
+                    </div>
                   </div>
                 </div>
-
-              </div>
-
-            )
-            )
-          )}
+              ))
+            )}
+          </div>
         </div>
-
-
-
-
-
-
-      </div>
+      </section>
 
 
 
--- src/pages/SingleMovie.jsx
+++ src/pages/SingleMovie.jsx
@@ -1,6 +1,55 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+
+
 export default function SingleMovie() {
 
+  const { id } = useParams()
+
+  const [movie, setMovie] = useState({})
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/v1/movies/' + id)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setMovie(data)
+      })
+      .catch(error => console.error(error))
+
+  }, [])
+
   return (
-    <h1>Single Movie</h1>
+    <>
+
+      <div className="jumbotron h-100">
+
+        <div className="p-5 mb-4 bg-trasparent rounded-3" >
+          <div className="container-fluid py-5">
+            <h1 className="display-5 fw-bolder mb-3">{movie.title}</h1>
+
+            <div className="row">
+              <div className="col-8 fs-3">
+                <p>Director: {movie.director}</p>
+                <p>Year: {movie.release_year}</p>
+                <p>Genre: {movie.genre}</p>
+                <div>
+                  Summary:
+                  <p>
+                    {movie.abstract}
+                  </p>
+                </div>
+              </div>
+
+              <div className="col-4">
+                <img src={`http://localhost:3000/images/${movie.image}`} alt="" className='img-fluid' />
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
