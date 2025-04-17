31cc4897da485c5618ab632c17458c35c951abb0
mattiacoli <mattia.coli@aol.com>
2025-04-17 17:09:45 +0200

    Add validation and error handling to FormReview component; display success and error messages

--- src/components/FormReview.jsx
+++ src/components/FormReview.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function FormReview({ movieId }) {
 
@@ -8,13 +9,41 @@ export default function FormReview({ movieId }) {
     text: ''
   }
 
-
   const [formData, setFormData] = useState(initialData)
+
+  const [success, setSuccess] = useState()
+
+  const [errors, setErrors] = useState([])
+
+  const navigate = useNavigate()
+
+
+
+  function isValidate(data) {
+    const formErrors = {}
+
+    if (data.name.length < 3) formErrors.name = 'Name must be at least 3 characters long'
+    if (!data.vote) formErrors.vote = "Vote is required"
+    if (data.vote < 1 || data.vote > 5) formErrors.vote = 'Vote must be between 1 and 5'
+    if (data.text.length < 10) formErrors.text = 'Your review must be at least 10 characters long '
+
+    setErrors(formErrors)
+
+    return Object.keys(formErrors).length === 0
+  }
 
 
 
   function handleSubmit(e) {
     e.preventDefault()
+
+    if (!isValidate(formData)) {
+
+      console.log({ errors });
+
+
+      return
+    }
 
     fetch('http://localhost:3000/api/v1/movies/' + movieId + '/review',
       {
@@ -30,20 +59,44 @@ export default function FormReview({ movieId }) {
       .then(data => {
 
         console.log(data);
+        setSuccess(data)
 
-      }).catch(error => console.log(error))
+        setTimeout(() => {
+          setSuccess(false)
+          navigate(0)
+        }, 2000)
 
-
-
-
+      })
+      .catch(error => console.log(error))
 
   }
-
 
   return (
     <>
 
       <div className="container">
+
+
+        {success && (
+
+          <div className="alert alert-success" role="alert">
+            {success.message}
+          </div>
+        )}
+
+        {Object.keys(errors).length > 0 && (
+
+          <div className="alert alert-danger" role="alert">
+            <ul>
+              {Object.keys(errors).map((key) => (
+                <li key={key}>
+                  {errors[key]}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+
         <form action="POST" onSubmit={handleSubmit}>
 
           <h2>Add your review</h2>
